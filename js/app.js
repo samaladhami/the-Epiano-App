d17e3ae3d1bcc43dae09fd0b2c1d8441a5660ba5
samer <samaladhami@gmail.com>
2016-09-15 21:26:35 -0500

    workingWithPiano

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-angular.module('ePiano' ,[])
+angular.module('ePiano' ,["ui.router"])
+.config(function($stateProvider , $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+  .state('home' , {
+    url: '/'
+    ,templateUrl: './js/templates/homeTemp.html'
+  })
+
+  .state('play' , {
+    url: '/play'
+    ,templateUrl: './js/templates/playTemp.html'
+  })
+})
